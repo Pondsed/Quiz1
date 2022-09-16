7f17c7fb1c0d63493ca43643496a20558c2c1998
Pondsed <111563844+Pondsed@users.noreply.github.com>
2022-09-16 16:39:12 +0700

    Add files via upload

---
+++ Train/Class.cs
@@ -0,0 +1,28 @@
+using System;
+
+    public class Compsognathus{
+        public string U_name;
+        public string U_Library;
+        private string U_Address;
+        public int U_Tel;
+        public string UA_name;
+        public string UA_surname;
+        private int ID_Card;
+        public double FT;
+        public double FT_Cost;
+
+        public Compsognathus (string U_name,string U_Library,string U_Address ,int U_Tel,string UA_name,string UA_surname,int ID_Card,double FT,double FT_Cost ){
+
+            this.U_name = U_name;
+            this.U_Library = U_Library;
+            this.U_Address = U_Address;
+            this.U_Tel = U_Tel;
+            this.UA_name = UA_name;
+            this.UA_surname = UA_surname;
+            this.ID_Card = ID_Card;
+            this.FT = FT;
+            this.FT_Cost = FT_Cost;
+        
+        }
+
+    }
